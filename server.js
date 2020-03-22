9389b0a267583283d1d891a73cc11fd8eb505ed4
Mariana Macedo <marianamrs1995@hotmail.com>
2020-03-22 15:13:21 -0300

    adding cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 const server = require("http").createServer(app);
+const cors = require('cors');
 const io = require("socket.io")(server)
 require('dotenv').config();
 
@@ -11,6 +12,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "public"));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
+
+app.use(cors({
+  credentials: true,
+  origin: ['https://resistencia-do-condado.herokuapp.com/', 'http://resistencia-do-condado.herokuapp.com/', 'http://localhost:3000']
+}));
 
 app.use('/api/game/' , require('./routes/game'))
 app.use('/', (req, res) => {
