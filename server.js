04d33b878934cf80b70b5691fb6ee0d5ea2dde8f
Mariana Macedo <marianamrs1995@hotmail.com>
2020-04-08 22:00:50 -0300

    descomentando

--- server.js
+++ server.js
@@ -10,39 +10,39 @@ const cors = require('cors');
 const logger = require("morgan")
 const Player = require('./src/models/Player');
 
-// //mongo
-// const url = process.env.MONGO_URL;
-// mongoose
-// .connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
-// .then(x => {
-//   console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-// })
-// .catch(err => {
-//   console.error('Error connecting to mongo', err)
-// })
+//mongo
+const url = process.env.MONGO_URL;
+mongoose
+.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(x => {
+  console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+})
+.catch(err => {
+  console.error('Error connecting to mongo', err)
+})
 
-// // Express View engine setup
-// app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, "public")));
-// app.set("views", path.join(__dirname, "public"));
-// app.engine("html", require("ejs").renderFile);
-// app.set("view engine", "html");
+// Express View engine setup
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
-// //cors
-// app.use(cors({
-//   credentials: true,
-//   origin: ['https://resistencia-do-condado.herokuapp.com/', 'http://resistencia-do-condado.herokuapp.com/', 'http://localhost:3000', 'http://localhost:3001']
-// }));
+//cors
+app.use(cors({
+  credentials: true,
+  origin: ['https://resistencia-do-condado.herokuapp.com/', 'http://resistencia-do-condado.herokuapp.com/', 'http://localhost:3000', 'http://localhost:3001']
+}));
 
-// // Middleware Setup
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// //Injected Routes
-// app.use('/api/game/' , require('./src/routes/game'));
-// app.use('/api/player/' , require('./src/routes/player'));
-// app.use('/api/room/' , require('./src/routes/room'));
+//Injected Routes
+app.use('/api/game/' , require('./src/routes/game'));
+app.use('/api/player/' , require('./src/routes/player'));
+app.use('/api/room/' , require('./src/routes/room'));
 app.use('/', (req, res) => {
     res.render('index.html');
 });
